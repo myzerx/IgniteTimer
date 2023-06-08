6b4ed24088d8436fc74239e87c0ed333bf8696fd
Richard Lemos <eternal.cassola@gmail.com>
2023-06-08 19:09:55 -0300

    wip(History): stylizing page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,48 @@
+import { HistoryContainer, HistoryList } from './style'
+
 export function History() {
   return (
-    <div>
-      <h1> History</h1>
-    </div>
+    <HistoryContainer>
+      <h1> Meu Historico </h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
